7860368454464b526721f27cd4c18838bd6e06e5
Eleazar Estrella <eestrella0425@gmail.com>
2019-05-10 10:09:01 -0400

    add favorite toggle to movieDetailViewController

--- Pelina Beer App/UI/MovieDetail/MovieDetailViewController.swift
+++ Pelina Beer App/UI/MovieDetail/MovieDetailViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Cosmos
 import Nuke
+import RxSwift
 
 class MovieDetailViewController: UIViewController {
     @IBOutlet weak var posterImageView: UIImageView!
@@ -16,8 +17,13 @@ class MovieDetailViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var favoriteToggle: UIButton!
     
     var movie: Movie?
+    
+    var viewModel: MovieCellViewModelType! = MovieCellViewModel()
+    
+    var disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,10 +32,19 @@ class MovieDetailViewController: UIViewController {
             fatalError("In order to use this viewController you must set the movie property")
         }
         
+        defer {
+            self.viewModel.movieAction.execute(movie)
+        }
+        
+        favoriteToggle.isSelected = false
+        favoriteToggle.setImage(#imageLiteral(resourceName: "favoriteIcon").withRenderingMode(.alwaysOriginal), for : .selected)
+        favoriteToggle.setImage(#imageLiteral(resourceName: "notFavoriteIcon").withRenderingMode(.alwaysOriginal), for: .normal)
+        
         self.titleLabel.text = movie.title
         self.descriptionTextView.text = movie.overview
         self.calificationBar.rating = movie.voteAverage/2
         self.calificationBar.isUserInteractionEnabled = false
+        
         Nuke.loadImage(with: URL(string: AppConfig.IMAGE_BASE_PATH + (movie.poster ?? ""))!,
                        options: ImageLoadingOptions(
                         transition: .fadeIn(duration: 0.33),
@@ -37,14 +52,25 @@ class MovieDetailViewController: UIViewController {
         ), into: self.posterImageView)
         
         
-        guard !movie.releaseDate.isEmpty else { return }
+        if !movie.releaseDate.isEmpty {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            
+            let date = dateFormatter.date(from: movie.releaseDate)
+            dateFormatter.dateFormat = "MMM d, yyyy"
+            
+            self.releaseDateLabel.text = "Release date: \(dateFormatter.string(from: date!))"
+        }
+
+        viewModel.favoriteMovieOutput
+            .subscribe(onNext: {
+                self.favoriteToggle.isSelected = $0.isFavorite
+            }).disposed(by: disposeBag)
         
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        
-        let date = dateFormatter.date(from: movie.releaseDate)
-        dateFormatter.dateFormat = "MMM d, yyyy"
-        
-        self.releaseDateLabel.text = "Release date: \(dateFormatter.string(from: date!))"
+    }
+    @IBAction func toggleFavoriteAction(_ sender: Any) {
+        let isFavorite = !favoriteToggle.isSelected
+        favoriteToggle.isSelected = isFavorite
+        viewModel.isFavoriteAction.execute(Favorite(movie: movie!, isFavorite: isFavorite))
     }
 }
